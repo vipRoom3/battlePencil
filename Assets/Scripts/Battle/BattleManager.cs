cbe7841ff1a1f6b33ae0d8b7f584d1a990b5faca
ZikaShi36 <deermanuniv@gmail.com>
2023-09-26 23:06:07 +0900

    タイトル画面とバトル画面のシーン追加

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 振るのは同時
+
+        // Battle ：一試合のこと
+        // RollFase ：バトペンを振ってアクションに移るまで
+        // ActionFase ：アクションが終わるまで
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
