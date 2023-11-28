4f5ae34b3ca11eac623c3b4a1c278478d28f20dc
ShizuQ <k.n.0825.h.k.0622@gmail.com>
2023-11-28 22:21:10 +0900

    ダメージ計算

--- Assets/Scripts/Battle/BattlePencilAction.cs
+++ Assets/Scripts/Battle/BattlePencilAction.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BattlePencilAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void BattleCalculation(Player myPlayer,Player enemyPlayer, int pencilNum)
     {
-
+        string[] arr =  myPlayer.pencil.ActionList[pencilNum - 1].Split(', ');
+        if(arr == "attack")
+        {
+            enemyPlayer.pencil.Hp = enemyPlayer.pencil.Hp - int.Parse(arr);
+        }
+        else if(arr == "heal")
+        {
+            myPlayer.pencil.Hp = myPlayer.pencil.Hp - int.Parse(arr);
+        }
+        else if(arr == "miss")
+        {
+            enemyPlayer.pencil.Hp = enemyPlayer.pencil.Hp - int.Parse(arr);
+        }
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-    }
-
 }
